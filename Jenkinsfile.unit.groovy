e5c56d84bb691ef1a8d20156faa1990786fece8b
root <root@ElAdmin>
2024-07-03 17:05:50 -0700

    implement some changes

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -3,6 +3,11 @@ pipeline {
         label 'docker'
     }
     stages {
+        stage('Source') {
+            steps {
+                git 'https://github.com/srayuso/unir-cicd'
+            }
+        }
         stage('Build') {
             steps {
                 echo 'Building stage!'
@@ -15,10 +20,35 @@ pipeline {
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
+        stage('API tests') {
+            steps {
+                sh 'make test-api'
+                archiveArtifacts artifacts: 'results/api_result.xml'
+            }
+        }
+        stage('E2E tests') {
+            steps {
+                sh 'make test-e2e'
+                archiveArtifacts artifacts: 'results/e2e/*.xml'
+            }
+        }
     }
     post {
         always {
             junit 'results/*_result.xml'
+            cleanWs()
+        }
+        failure {
+            script {
+                def jobName = env.JOB_NAME
+                def buildNumber = env.BUILD_NUMBER
+                def subject = "Pipeline ${jobName} failed at build ${buildNumber}"
+                def body = "Job ${jobName} build ${buildNumber} failed. Check Jenkins for more details."
+                echo "Sending email notification..."
+                mail to: 'martin.cuevas.t@gmail.com',
+                     subject: subject,
+                     body: body
+            }
         }
     }
 }
